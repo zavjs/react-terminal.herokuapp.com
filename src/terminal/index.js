12567a553a7f3d1f1cf6eefa2796c5febccf5f46
Henrique Zavareze <henrique@zavareze.com.br>
2018-10-13 23:28:07 -0300

    creating multiple files, contract fix

--- src/terminal/index.js
+++ src/terminal/index.js
@@ -1,21 +1,30 @@
 const terminal_utils = require("../utils/terminal");
 
 const terminal = (tree: Object) => {
+  const handleTerminalResp = response => {
+    if (typeof response === "object") {
+      return {
+        updated: response
+      };
+    } else {
+      return {
+        output: response
+      };
+    }
+  };
+
   const touch = function(fileName: string) {
     const parts = terminal_utils.get_file_path(fileName);
-    const file: string = terminal_utils.get_file_name(fileName);
+    const file = terminal_utils.get_file_name(fileName);
     const updatedTree = terminal_utils.makeFile(tree, parts, file);
-    return {
-      updated: updatedTree
-    };
+
+    return handleTerminalResp(updatedTree);
   };
 
   const mkdir = function(filePath: string) {
     const parts = terminal_utils.get_contents_list(filePath);
     const updatedTree = terminal_utils.makeTree(tree, parts);
-    return {
-      updated: updatedTree
-    };
+    return handleTerminalResp(updatedTree);
   };
 
   const ls = function() {
